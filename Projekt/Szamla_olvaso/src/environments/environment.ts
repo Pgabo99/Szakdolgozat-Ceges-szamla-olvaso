f8f9ac5e52cafc16b98437e81cc2356e367e0c83
Poprádi Gabriella <h268517@stud.u-szeged.hu>
2024-09-30 22:12:04 +0200

    Szakdolgozat pici szépítése

--- Projekt/Szamla_olvaso/src/environments/environment.ts
+++ Projekt/Szamla_olvaso/src/environments/environment.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "your-api-key",
+      authDomain: "your-auth-domain",
+      projectId: "your-project-id",
+      storageBucket: "your-storage-bucket",
+      messagingSenderId: "your-messaging-sender-id",
+      appId: "your-app-id",
+    }
+  };
